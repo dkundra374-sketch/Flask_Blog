0b713ebabc3a8fc3c27f79140fbb8d12e6d3d8cb
Disha Kundra <admin@192.168.1.20>
2026-07-14 19:12:19 +0530

    Add templates, Jinja2, Bootstrap and template inheritance

--- app.py
+++ app.py
@@ -1,15 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template , url_for 
 
 app = Flask(__name__)
+posts = [ 
+    { 
+        'author': 'Disha Kundra' ,
+        'title' : 'Blog Post 1'  ,
+        'content' : 'First post content' ,
+        'date_posted' : '14 July 2026'
+    } ,
+    {   'author': 'Abc Xyz' ,
+        'title' : 'Blog Post 2'  ,
+        'content' : 'Second post content' ,
+        'date_posted' : '14 July 2026'
+    }
 
+]
+ 
 @app.route("/")
 @app.route("/home")
 def home():
-    return " <h1>Hello, Flask! </h1> "
+    return render_template('home.html', posts=posts)
 
 @app.route("/about")
 def about():
-    return " <h1>About page! </h1> "
+    return render_template('about.html', title='About')
 
 if __name__ == "__main__":
     app.run(debug=True)
